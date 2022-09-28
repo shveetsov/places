1bea1041e6470a4711f2e728b9148e69dc0952e9
shveetsov <kirillshveetsov@gmail.com>
2022-09-28 22:21:10 +1200

    4.4-text Сделал 4.4

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,9 +10,25 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            color: Color.fromRGBO(37, 40, 73, 1),
+            fontFamily: 'Roboto',
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        toolbarHeight: 100,
+        backgroundColor: Colors.white,
+        shadowColor: const Color.fromRGBO(255, 255, 255, 0),
+      ),
       body: Center(
-          child: Text('Hello!'),
+        child: Text('Hello!'),
       ),
     );
   }
