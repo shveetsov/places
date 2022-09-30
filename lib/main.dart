2540ca3ad1b74ed0313cfee79dde1b36171964c6
shveetsov <kirillshveetsov@gmail.com>
2022-09-30 21:49:28 +1200

    4.8-size Ответ на вопрос, текст стал в 2 раза меньше так как взял размер от родителя. ConstrainedBox задаёт диапазон размеров в котором может находиться ребёнок. Скриншот прилагаю

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:places/mocks.dart';
-import 'package:places/ui/screen/sight_details.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,53 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SightDetails(sight: mocks[0],),
+      home: SightListScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
   }
 }
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -14,20 +14,23 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: false,
-        title: const Text(
-          'Список\nинтересных мест',
-          style: TextStyle(
-            color: Color.fromRGBO(37, 40, 73, 1),
-            fontFamily: 'Roboto',
-            fontSize: 32,
-            fontWeight: FontWeight.w700,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: AppBar(
+          centerTitle: false,
+          title: const Text(
+            'Список\nинтересных мест',
+            style: TextStyle(
+              color: Color.fromRGBO(37, 40, 73, 1),
+              fontFamily: 'Roboto',
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+            ),
           ),
+          toolbarHeight: 100,
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-        toolbarHeight: 100,
-        backgroundColor: Colors.white,
-        shadowColor: const Color.fromRGBO(255, 255, 255, 0),
       ),
       body: ListView.builder(
         padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
